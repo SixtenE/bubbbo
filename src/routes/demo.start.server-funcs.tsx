739b7b2b0a1e73d67e31596f4bb8c64daade55af
SixtenE <sixten.ekblad@gmail.com>
2025-07-25 22:07:54 +0200

    init

---
+++ src/routes/demo.start.server-funcs.tsx
@@ -0,0 +1,42 @@
+import { createFileRoute, useRouter } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+
+async function readCount() {
+  return 1
+}
+
+const getCount = createServerFn({
+  method: 'GET',
+}).handler(() => {
+  return readCount()
+})
+
+const updateCount = createServerFn({ method: 'POST' })
+  .validator((d: number) => d)
+  .handler(async () => {})
+
+export const Route = createFileRoute('/demo/start/server-funcs')({
+  component: Home,
+  loader: async () => await getCount(),
+})
+
+function Home() {
+  const router = useRouter()
+  const state = Route.useLoaderData()
+
+  return (
+    <div className="p-4">
+      <button
+        type="button"
+        onClick={() => {
+          updateCount({ data: 1 }).then(() => {
+            router.invalidate()
+          })
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Add 1 to {state}?
+      </button>
+    </div>
+  )
+}
